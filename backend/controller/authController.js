05f23b1815070ef9f71c9f21d16d2153c6dc84cd
developerphanom12 <developer.phanomprofessionals@gmail.com>
2025-04-22 11:23:47 +0530

    Add frontend folder to main reposityry

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -7,7 +7,7 @@ const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    const exists = await User.findOne({ email });
+    const exists = await User.findOne({ email }); 
     if (exists) return res.status(400).json({ msg: 'User already exists' });
 
     const hash = await bcrypt.hash(password, 10);
